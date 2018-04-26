6f1bd64334c9fd6122d312c307a958abadba693e
Petrov Réka <36596106+lplx55@users.noreply.github.com>
2018-04-26 16:17:46 +0200

    Add files via upload

--- ex_7.py
+++ ex_7.py
@@ -1,11 +1,27 @@
 def longestPalindrom(str):
-#gives back the longest palindromic substring of given string
+#returns the longest palindromic substring of given string
     max=''
-    for i in range(0,len(str)//2):
-        for j in range(i, len(str)+1,-1):
+    for i in range(0,len(str)):
+        for j in range(0,len(str),-1):
             if str[i]==str[j]:
                 if len(str[i:j])>len(max):
                     max=str[i:j]
+                print(str[i:]+str[:j])
     return max
+
+def longestPalindrom2(str):
+    i=0
+    j=len(str)-1
+    max=[]
+    maxl=0
+    while i<=len(str) and j>0:
+        if str[i]==str[j]:
+            max.append(str[i:j+1])
+        i+=1
+        j-=1
+    for i in max:
+        if len(i)>maxl:
+            maxl=len(i)
+        return i
 #main
-print(longestPalindrom("gag"))
+print(longestPalindrom2("arab"))
