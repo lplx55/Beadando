a12cda22dd9abf33f968a022ccd75db02266b9a9
Petrov Réka <36596106+lplx55@users.noreply.github.com>
2018-05-04 15:43:17 +0200

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,25 +1,3 @@
-'''def convert2(lst,b1,b2):
-    n=''
-    L=[]
-    for i in lst:
-        i=str(i)
-        n+=i
-    n=int(n)
-    if b1==10:
-        while n>0:
-            l=n%b2
-            n//=b2
-            L.append(l)
-        return [i for i in L[::-1]]
-    elif b1==2:
-        sum=0
-        lst=lst[::-1]
-        for k in range(0,len(lst)):
-            sum+=lst[k]*b1**k
-        return sum
-    else:
-        return'''
-
 def convert(lst,b1,b2):
     if b1!=10:
         B10=[]
